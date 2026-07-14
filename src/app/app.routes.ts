e56f02ffd9c3c9f4fa2459d65fc4882582a15176
Soulmateafk <etdjohancerinza@gmail.com>
2026-07-14 12:56:15 -0500

    dashboard excel13

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,7 +12,7 @@ import { AlertasComponent } from './pages/alertas/alertas';
 import { CombustibleComponent } from './components/combustible/combustible';
 import { MapaEntregasComponent } from './pages1/mapa-entregas/mapa-entregas';
 import { DashexcComponent } from './dasboardexcel/dashexc';
-import { DashSemanalComponent } from './dashboard-semanal/dash-semanal.component';
+import { DashSemanalComponent } from './dashboard-semanal/dashSem';
 
 // IMPORTAMOS EL GUARDIA QUE CREAMOS
 import { authGuard } from './login/auth.guard';
