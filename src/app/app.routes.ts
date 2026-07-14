e5fc5e32d32431636dd11c658b6ca603d37f8c67
Soulmateafk <etdjohancerinza@gmail.com>
2026-07-14 14:16:19 -0500

    dashboard excel16

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -33,5 +33,5 @@ export const routes: Routes = [
   { path: 'combustible', component: CombustibleComponent, canActivate: [authGuard] },
   { path: 'mapa', component: MapaEntregasComponent, canActivate: [authGuard] },
   { path: 'dash-entregas', component: DashexcComponent, canActivate: [authGuard] },
-  { path: 'reporte-semanal', component: ReporteComponent, canActivate: [authGuard] }
+  { path: 'reporte-semanal', component: ReporteSemanalComponent, canActivate: [authGuard] }
 ];
